23a8a3420640c28da351be8261a3486cce096cb2
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-19 19:09:38 +0200

    add useEffect and ensure it runs once, add fetchPost function to fetch data from API from the URL, check if response is successful, implement try catch error to handle errors, if there is an error a message will be displayed

--- blog-fetcher/src/BlogPost.jsx
+++ blog-fetcher/src/BlogPost.jsx
@@ -6,4 +6,23 @@ const BlogPosts = () => {
     const [post, setPost] = useState([]);
     const [error, setError] = useState(null);
 
+    useEffect(() => {
+
+        //fetch API call
+        const fetchPost = async () => {
+            try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+                if(!response.ok) {
+                    throw new Error('Data fetching failed');
+                }
+                const data = await response.json();
+                setPost(data);
+            }   catch(err) {
+                setError(err.message);
+            }
+        };
+        
+        fetchPost();
+    }, []); //empty dependency array ensures this runs only once
+
 }
