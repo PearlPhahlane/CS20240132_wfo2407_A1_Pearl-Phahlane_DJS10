30547f915cd387307cfcd89e686e235fcfa9e48d
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-19 19:01:32 +0200

    set post and error state variables, post will store array of blog posts fetched from API and error will store error messages if API call fails

--- blog-fetcher/src/BlogPost.jsx
+++ blog-fetcher/src/BlogPost.jsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const BlogPosts = () => {
+
+    //state management 
+    const [post, setPost] = useState([]);
+    const [error, setError] = useState(null);
+
 }
